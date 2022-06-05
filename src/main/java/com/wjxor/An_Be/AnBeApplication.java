38f2a9897049506d2e87dd0c760cc538a0fb643f
wjxor <shko940575@gmail.com>
2022-06-06 00:37:38 +0900

    최초커밋

---
+++ src/main/java/com/wjxor/An_Be/AnBeApplication.java
@@ -0,0 +1,13 @@
+package com.wjxor.An_Be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjxor/An_Be/AnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjxor.An_Be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
